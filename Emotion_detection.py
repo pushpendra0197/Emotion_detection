7ff54ac03ac1e0ea9745d6fb47af0c7893cd8c44
Pushpendra Dhamanya <162273949+pushpendra0197@users.noreply.github.com>
2025-01-01 12:40:30 +0530

    Update Emotion_detection.py

--- Emotion_detection.py
+++ Emotion_detection.py
@@ -31,7 +31,7 @@ camera=cv2.VideoCapture(0)
 if button:
    try:
     while True:
-      _,frame=camera.read(0)
+      _,frame=camera.read()
       frame=cv2.flip(frame,1)
       gray=cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
       faces=face_c.detectMultiScale(gray,1.7)
